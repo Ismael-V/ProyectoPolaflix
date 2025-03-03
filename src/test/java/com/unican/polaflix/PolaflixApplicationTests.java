8993f30b5da09185aaacee117f5fbe173bd9717e
Ismael-V <ismaelpsabi@gmail.com>
2025-03-03 22:14:43 +0100

    Comienzo de Polaflix

---
+++ src/test/java/com/unican/polaflix/PolaflixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unican.polaflix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PolaflixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
